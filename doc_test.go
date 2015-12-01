2e1d9d8d0d9308bb0545b18e54814e82a02bba34
Thomas Lokshall <thomas.reinsbo.lokshall@atea.no>
2015-12-01 12:22:37 +0100

    Move package overview into doc.go

--- doc_test.go
+++ doc_test.go
@@ -1,28 +1,3 @@
-// Package match implements several Matchers for use in unit testing.
-//
-// Chaining
-//
-// The matchers can be used individually or chained together to make complex
-// statements.
-//     func TestComplexStatement(t *testing.T) {
-//         value := DoSomethingThatReturns()
-//         match.IsNotNil(t, value).KindOf(reflect.String)
-//     }
-//
-// Fatal Errors
-//
-// In order to handle fatal errors where further tests should not be
-// executed, there is a .Fatal function that will cause the test to fail
-// immediately.
-// If .Fatal is used in a chain, it will only apply to the previous statements
-// and not the subsequent ones.
-//     func TestFatalError(t *testing.T) {
-//         value := DoSomethingThatReturns()
-//         match.IsNotNil(t, value).Fatal().KindOf(reflect.String)
-//         DoSomethingWith(value)
-//     }
-// In the above example, the test will fail immediately if value is nil, but
-// will continue if the value is not a string.
 package match
 
 import (
