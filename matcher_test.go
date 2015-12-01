e031d9e82011548cb8a51215272bc97620f77a6e
Thomas Lokshall <thomas.reinsbo.lokshall@atea.no>
2015-12-01 11:13:10 +0100

    Fix recover in matcher.go tests that panic

--- matcher_test.go
+++ matcher_test.go
@@ -6,199 +6,219 @@ import (
 )
 
 func TestMatcherFatal(t *testing.T) {
-	test := &testing.T{}
-	m := Matcher{test, 0, false}
-	m.Equals(0).Fatal().Equals(1)
-	if test.Failed() {
-		t.Error("Expected pass, but the test failed")
-	}
-	m.Equals(1).Fatal().Equals(0)
-	if !test.Failed() {
-		t.Error("Expected failure, but the test passed")
-	}
+	done := make(chan bool)
+	go func() {
+		test := &testing.T{}
+		m := Matcher{test, 0}
+		defer func() {
+			recover()
+			if !test.Failed() {
+				t.Error("expected failure, but the test passed")
+			}
+			done <- true
+		}()
+		m.Equals(1).Fatal().Equals(0)
+	}()
+	<-done
 }
 
 func TestMatcherIsNil(t *testing.T) {
 	test := &testing.T{}
-	m := Matcher{test, nil, false}
+	m := Matcher{test, nil}
 	m.IsNil()
 	if test.Failed() {
-		t.Error("Expected pass, but the test failed")
+		t.Error("expected pass, but the test failed")
 	}
-	m = Matcher{test, 0, false}
+	m = Matcher{test, 0}
 	m.IsNil()
 	if !test.Failed() {
-		t.Error("Expected failure, but the test passed")
+		t.Error("expected failure, but the test passed")
 	}
 }
 
 func TestMatcherIsNotNil(t *testing.T) {
 	test := &testing.T{}
-	m := Matcher{test, 0, false}
+	m := Matcher{test, 0}
 	m.IsNotNil()
 	if test.Failed() {
-		t.Error("Expected pass, but the test failed")
+		t.Error("expected pass, but the test failed")
 	}
-	m = Matcher{test, nil, false}
+	m = Matcher{test, nil}
 	m.IsNotNil()
 	if !test.Failed() {
-		t.Error("Expected failure, but the test passed")
+		t.Error("expected failure, but the test passed")
 	}
 }
 
 func TestMatcherEquals(t *testing.T) {
 	test := &testing.T{}
-	m := Matcher{test, 0, false}
+	m := Matcher{test, 0}
 	m.Equals(0)
 	if test.Failed() {
-		t.Error("Expected pass, but the test failed")
+		t.Error("expected pass, but the test failed")
 	}
-	m = Matcher{test, nil, false}
+	m = Matcher{test, nil}
 	m.Equals(0)
 	if !test.Failed() {
-		t.Error("Expected failure, but the test passed")
+		t.Error("expected failure, but the test passed")
 	}
 }
 
 func TestMatcherNotEquals(t *testing.T) {
 	test := &testing.T{}
-	m := Matcher{test, nil, false}
+	m := Matcher{test, nil}
 	m.NotEquals(0)
 	if test.Failed() {
-		t.Error("Expected pass, but the test failed")
+		t.Error("expected pass, but the test failed")
 	}
-	m = Matcher{test, 0, false}
+	m = Matcher{test, 0}
 	m.NotEquals(0)
 	if !test.Failed() {
-		t.Error("Expected failure, but the test passed")
+		t.Error("expected failure, but the test passed")
 	}
 }
 
 func TestMatcherLessThan(t *testing.T) {
 	test := &testing.T{}
-	m := Matcher{test, 0, false}
+	m := Matcher{test, 0}
 	m.LessThan(1)
 	if test.Failed() {
-		t.Error("Expected pass, but the test failed")
+		t.Error("expected pass, but the test failed")
 	}
-	m = Matcher{test, 2, false}
+	m = Matcher{test, 2}
 	m.LessThan(1)
 	if !test.Failed() {
-		t.Error("Expected failure, but the test passed")
+		t.Error("expected failure, but the test passed")
 	}
 }
 
 func TestMatcherLessThan_NonNumeric(t *testing.T) {
-	test := &testing.T{}
-	m := Matcher{test, "0", false}
-	m.LessThan(1)
-	if !test.Failed() {
-		t.Error("Expected failure, but the test passed")
-	}
+	done := make(chan bool)
+	go func() {
+		test := &testing.T{}
+		m := Matcher{test, "0"}
+		defer func() {
+			recover()
+			if !test.Failed() {
+				t.Error("expected failure, but the test passed")
+			}
+			done <- true
+		}()
+		m.LessThan(1)
+	}()
+	<-done
 }
 
 func TestMatcherGreaterThan(t *testing.T) {
 	test := &testing.T{}
-	m := Matcher{test, 2, false}
+	m := Matcher{test, 2}
 	m.GreaterThan(1)
 	if test.Failed() {
-		t.Error("Expected pass, but the test failed")
+		t.Error("expected pass, but the test failed")
 	}
-	m = Matcher{test, 0, false}
+	m = Matcher{test, 0}
 	m.GreaterThan(1)
 	if !test.Failed() {
-		t.Error("Expected failure, but the test passed")
+		t.Error("expected failure, but the test passed")
 	}
 }
 
 func TestMatcherGreaterThan_NonNumeric(t *testing.T) {
-	test := &testing.T{}
-	m := Matcher{test, "2", false}
-	m.GreaterThan(1)
-	if !test.Failed() {
-		t.Error("Expected failure, but the test passed")
-	}
+	done := make(chan bool)
+	go func() {
+		test := &testing.T{}
+		m := Matcher{test, "2"}
+		defer func() {
+			recover()
+			if !test.Failed() {
+				t.Error("expected failure, but the test passed")
+			}
+			done <- true
+		}()
+		m.GreaterThan(1)
+	}()
+	<-done
 }
 
 func TestMatcherContains_Array(t *testing.T) {
 	test := &testing.T{}
-	m := Matcher{test, []int{0, 1, 2, 3}, false}
+	m := Matcher{test, []int{0, 1, 2, 3}}
 	m.Contains(1)
 	if test.Failed() {
-		t.Error("Expected pass, but the test failed")
+		t.Error("expected pass, but the test failed")
 	}
 	m.Contains(4)
 	if !test.Failed() {
-		t.Error("Expected failure, but the test passed")
+		t.Error("expected failure, but the test passed")
 	}
 }
 
 func TestMatcherContains_Map(t *testing.T) {
 	test := &testing.T{}
-	m := Matcher{test, map[string]int{"a": 0, "b": 1}, false}
+	m := Matcher{test, map[string]int{"a": 0, "b": 1}}
 	m.Contains("b")
 	if test.Failed() {
-		t.Error("Expected pass, but the test failed")
+		t.Error("expected pass, but the test failed")
 	}
 	m.Contains("c")
 	if !test.Failed() {
-		t.Error("Expected failure, but the test passed")
+		t.Error("expected failure, but the test passed")
 	}
 }
 
 func TestMatcherContains_String(t *testing.T) {
 	test := &testing.T{}
-	m := Matcher{test, "abc", false}
+	m := Matcher{test, "abc"}
 	m.Contains("bc")
 	if test.Failed() {
-		t.Error("Expected pass, but the test failed")
+		t.Error("expected pass, but the test failed")
 	}
 	m.Contains("cd")
 	if !test.Failed() {
-		t.Error("Expected failure, but the test passed")
+		t.Error("expected failure, but the test passed")
 	}
 }
 
 func TestMatcherMatches(t *testing.T) {
 	test := &testing.T{}
-	m := Matcher{test, "this is a test", false}
+	m := Matcher{test, "this is a test"}
 	m.Matches("^this is a test$")
 	if test.Failed() {
-		t.Error("Expected pass, but the test failed")
+		t.Error("expected pass, but the test failed")
 	}
 	m.Matches("^this is not a test$")
 	if !test.Failed() {
-		t.Error("Expected failure, but the test passed")
+		t.Error("expected failure, but the test passed")
 	}
 }
 
 func TestMatcherMatches_InvalidRegex(t *testing.T) {
 	test := &testing.T{}
-	m := Matcher{test, 0, false}
+	m := Matcher{test, 0}
 	m.Matches("^0$")
 	if !test.Failed() {
-		t.Error("Expected failure, but the test passed")
+		t.Error("expected failure, but the test passed")
 	}
 }
 
 func TestMatcherMatches_NonString(t *testing.T) {
 	test := &testing.T{}
-	m := Matcher{test, "this is a test", false}
+	m := Matcher{test, "this is a test"}
 	m.Matches("(?x:^this is a test$)")
 	if !test.Failed() {
-		t.Error("Expected failure, but the test passed")
+		t.Error("expected failure, but the test passed")
 	}
 }
 
 func TestMatcherKindOf(t *testing.T) {
 	test := &testing.T{}
-	m := Matcher{test, 0, false}
+	m := Matcher{test, 0}
 	m.KindOf(reflect.Int)
 	if test.Failed() {
-		t.Error("Expected pass, but the test failed")
+		t.Error("expected pass, but the test failed")
 	}
 	m.KindOf(reflect.String)
 	if !test.Failed() {
-		t.Error("Expected failure, but the test passed")
+		t.Error("expected failure, but the test passed")
 	}
 }
